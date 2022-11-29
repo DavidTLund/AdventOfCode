304c0daee208922f1ebc9ee212651c4243e6ff2f
David Lund <david.lund@knowit.se>
2022-11-29 18:06:43 +0100

    Save Day 4 2015

--- 2015/Day04IdealStockingStuffer.py
+++ 2015/Day04IdealStockingStuffer.py
@@ -1,15 +1,33 @@
 import hashlib
 
+
+def find_first_MD5(key):
+    # initializing string
+    index = 0
+    keepLooking = True
+    hashPrefix = "000000"
+    hashToTest = ""
+    while keepLooking:
+        index += 1
+        str2hash = key + str(index)
+        result = hashlib.md5(str2hash.encode())
+        hashToTest = result.hexdigest()
+        if hashPrefix == hashToTest[0:6]:
+            print("hash string", str2hash)
+            print("The hexadecimal equivalent of hash is : ", end ="")
+            print(result.hexdigest())
+            keepLooking = False
+            return index
+    return -1
+
+        
+
+secret_key = 'abcdef'
+secret_key = 'pqrstuv'
+
+# David Lund Knowit key. Funny thing the answer was the same for both
 secret_key = 'bgvyzdsv'
 
-# initializing string
-str2hash = secret_key + "0000001"
-  
-str2hash = 'abcdef609043'
-# encoding GeeksforGeeks using encode()
-# then sending to md5()
-result = hashlib.md5(str2hash.encode())
-  
-# printing the equivalent hexadecimal value.
-print("The hexadecimal equivalent of hash is : ", end ="")
-print(result.hexdigest())
+index = find_first_MD5(secret_key)
+
+print("The smallest int:", index)
