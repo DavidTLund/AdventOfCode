bf2fd9d8ffff5892ae1b613f67e675bd069ea800
David Lund <david.lund@knowit.se>
2022-12-04 20:01:58 +0100

    Fixing getdata

--- 2022/Day03RucksackReorg.py
+++ 2022/Day03RucksackReorg.py
@@ -18,14 +18,38 @@ def findCommonChInRow(l, r):
             return ch
 
 def scoreLetter(ch):
+    if ch == None:
+        return 0
     if ch.islower():
         return ord(ch) - ord('a') + 1
     return ord(ch) - ord('A') + 27
 
+def getData():
+    with open(data_file) as f:
+        alltext =f.read()
+        rucksacks = alltext.split('\n')
+        return rucksacks
+
 def main_part1():
     score = 0
+    rucksacks = getData()
+    for rucksack in rucksacks:
+        rucksack = rucksack.rstrip()
+        sides = splitRuckSack(rucksack)
+        #print('Rucksack:', rucksack, sides)
+        ch = findCommonChInRow(sides[0], sides[1])
+        tscore = scoreLetter(ch)
+        #print('Letter', ch, tscore)
+        score += tscore
+    print('Score:', score)
+
+def main_part2():
+    score = 0
     with open(data_file) as f:
-        for rucksack in f:
+        alltext =f.read()
+        rucksacks = alltext.split('\r\n')
+
+        for rucksack in rucksacks:
             rucksack = rucksack.rstrip()
             sides = splitRuckSack(rucksack)
             print('Rucksack:', rucksack, sides)
@@ -34,6 +58,5 @@ def main_part1():
             print('Letter', ch, tscore)
             score += tscore
     print('Score:', score)
-
 
 main_part1()
